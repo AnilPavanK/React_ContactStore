7fdc673e75e33e7dda197cc70443cb5eead4a830
Anil Pavan <appkumark@yahoo.com>
2019-11-06 04:43:33 +0530

    Complete full stack font-to-back using Mongo,Express,React & Node

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,20 +1,57 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import AuthContext from '../../context/auth/AuthContext';
+import ContactContext from '../../context/contact/ContactContext';
 
 const Navbar = ({ title, icon }) => {
+  const authContext = useContext(AuthContext);
+  const contactContext = useContext(ContactContext);
+
+  const { isAuthenticated, logOut, user } = authContext;
+  const { clearContacts } = contactContext;
+
+  const onLogout = () => {
+    logOut();
+    clearContacts();
+  };
+
+  const authLinks = (
+    <Fragment>
+      <li>Hello {user && user.name}</li>
+      <li>
+        <a onClick={onLogout} href='#!'>
+          <i className='fa fa-sign-out'></i>{' '}
+          <span className='hide-sm'>Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to='/register'>Register</Link>
+      </li>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+    </Fragment>
+  );
+
   return (
     <div className='navbar bg-primary'>
       <h1>
         <i className={icon} /> {title}
       </h1>
       <ul>
-        <li>
+        {/* <li>
           <Link to='/'>Home</Link>
         </li>
         <li>
           <Link to='/about'>About</Link>
-        </li>
+        </li> */}
+        {isAuthenticated ? authLinks : guestLinks}
       </ul>
     </div>
   );
--- routes/contacts.js
+++ routes/contacts.js
@@ -64,15 +64,55 @@ router.post(
 // @route     PUT  api/contacts/:id
 // @desc      Update contact
 // @access    Private
-router.put('/:id', (req, res) => {
-  res.send('Update contact');
+// router.put('/:id', (req, res) => {
+//   res.send('Update contact');
+// });
+
+router.put('/:id', auth, async (req, res) => {
+  const { name, email, phone, type } = req.body;
+
+  // Build contact object
+  const contactFields = {};
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    if (!contact) return res.status(404).json({ msg: 'Contact not found' });
+
+    // Make sure user owns contact
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      { $set: contactFields },
+      { new: true }
+    );
+
+    res.json(contact);
+  } catch (err) {
+    console.error(er.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route     DELETE  api/contacts/:id
 // @desc      Delete contact
 // @access    Private
 router.delete('/:id', (req, res) => {
-  res.send('Delete contact');
+  // res.send('Delete contact');
+  Contact.deleteOne({ _id: req.params.id })
+    .then(() => {
+      res.status(200).json({ message: 'Deleted!' });
+    })
+    .catch(error => {
+      res.status(400).json({ error: error });
+    });
 });
 
 module.exports = router;
